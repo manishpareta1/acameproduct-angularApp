8998fdc5469c534c5e9c35a5fb0f348ba7b30072
manishpareta1 <manishpareta1@gmail.com>
2019-08-29 16:26:42 +0530

    Angular app for Acame Products

---
+++ src/app/app.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+
+import { AuthService } from './user/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'pm-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  pageTitle = 'Acme Product Management';
+  
+  
+  get isLoggedIn(): boolean {
+    return this.authService.isLoggedIn;
+  }
+
+  get userName(): string {
+    if (this.authService.currentUser) {
+      return this.authService.currentUser.userName;
+    }
+    return '';
+  }
+
+  constructor(private authService: AuthService,private route: Router) { }
+
+  logOut(): void {
+    this.authService.logout();
+    console.log('Log out');
+    //its good to use navigateByUrl then navigate method, coz we are logging out so we should
+    //redirect user to home url not to any intermident url
+    this.route.navigateByUrl('/welcome');
+
+  }
+}
---
+++ src/app/page-not-found.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  template: `    
+    <img src="./assets/images/pageNotFound.jpg"
+             class="img-responsive center-block"
+             style="max-height:500px;padding-bottom:100px" />
+    `
+})
+export class PageNotFoundComponent { }
